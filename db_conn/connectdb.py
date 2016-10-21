24d87e17e4e43340e3d687fc6bdbfe9951a3abc7
JackLau <lj2000hjy@sohu.com>
2016-10-21 07:44:49 +0000

    添加Mysql编码,防止乱码的产生

--- db_conn/connectdb.py
+++ db_conn/connectdb.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # _*_ coding:UTF-8 _*_
-
+# import sys
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
 # 开启数据库服务：sudo service mysql start
 import MySQLdb as mdb
 # 创建连接,打开数据库连接
@@ -8,7 +10,12 @@ db = mdb.connect(host='127.0.0.1',user='root',passwd='',db='test')
 # 使用cursor()方法获取操作游标 
 cursor=db.cursor()
 # 使用execute方法执行SQL语句
-cursor.execute("SELECT VERSION()")
+#cursor.execute("SELECT VERSION()")
+#设置为utf-8编码
+cursor.execute("SET NAMES utf8;")
+#cursor.execute('SET CHARACTER SET utf8;')
+#cursor.execute('SET character_set_connection=utf8;')
+
 # 使用 fetchone() 方法获取一条数据。
 data = cursor.fetchone()
 print "Database version : %s " % data
